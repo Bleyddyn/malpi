248577c23e53c58199fb8541d4617c7de24ba4b3
Andrew Salamon <bleyddyn.aprhys@gmail.com>
2017-12-07 15:03:14 -0800

    Added command arguments and lots of features for post-processing drive data.

--- tf/post/drive_desc.py
+++ tf/post/drive_desc.py
@@ -1,12 +1,21 @@
 import os
 import pickle
+import argparse
 from scipy.misc import imresize
+import matplotlib.pyplot as plt
+import numpy as np
 
-def dumpArrayToFile( data, key ):
-    fname = key + ".pickle"
+def dumpArrayToFile( data, key, fname=None, indent="" ):
+    if not fname:
+        fname = key + ".pickle"
+    if not key in data:
+        print( "{}Missing '{}' key in drive data.".format( indent, key ) )
+        return False
+
     array = data[key]
     with open(fname, 'w') as f:
         pickle.dump( array, f, protocol=pickle.HIGHEST_PROTOCOL )
+    return True
 
 def readArrayFromFile( key ):
     fname = key + ".pickle"
@@ -24,51 +33,48 @@ def getImagesFrom( filename ):
         small.append(img)
     return small
 
-if not os.path.exists('image_actions.pickle'):
-    with open('drive.pickle','r') as f:
+def loadDrive( drive_dir, indent="" ):
+    ifname = os.path.join( drive_dir, 'drive.pickle' )
+    if not os.path.exists(ifname):
+        print( "{}No drive.pickle. Possibly an older drive format".format( indent ) )
+        return None
+
+    with open(ifname,'r') as f:
         data = pickle.load(f)
-    print( "{} Image actions".format( len(data['image_actions'] ) ) )
-    dumpArrayToFile( data, 'image_actions' )
-else:
-    print( "Image actions already exists" )
 
-if not os.path.exists('images_120x120.pickle'):
-    smallImages = []
-    for fname in os.listdir("."):
-        if fname.startswith("images_") and fname.endswith(".pickle"):
-            print( "Reading {}".format(fname) )
-            im1 = getImagesFrom(fname)
-            smallImages.extend(im1)
+    return data
 
-    print( "writing {} small images".format( len(smallImages) ) )
+def makeActions( drive_dir, data, indent="" ):
+    ofname = os.path.join( drive_dir, 'image_actions.pickle' )
+    if not os.path.exists(ofname):
+        if dumpArrayToFile( data, 'image_actions', fname=ofname ):
+            print( "{}Saved {} Image actions".format( indent, len(data['image_actions'] ) ) )
+    else:
+        print( "{}Image actions already exists".format( indent ) )
 
-    with open('images_120x120.pickle','wb') as f:
-        pickle.dump( smallImages, f, protocol=pickle.HIGHEST_PROTOCOL )
-else:
-    print( "Small Images file already exists" )
+def makeSmallImages( drive_dir, data, indent="" ):
+    ofname = os.path.join( drive_dir, 'images_120x120.pickle' )
+    if not os.path.exists(ofname):
+        smallImages = []
+        for fname in os.listdir(drive_dir):
+            if fname.startswith("images_") and fname.endswith(".pickle"):
+                ifname = os.path.join( drive_dir, fname )
+                print( "{}Reading {}".format( indent, fname ) )
+                im1 = getImagesFrom(ifname)
+                smallImages.extend(im1)
 
-#with open('drive.pickle','r') as f:
-#    data = pickle.load(f)
+        if len(smallImages) == 0 and 'images' in data:
+            img_data = data['images']
+            for img in img_data:
+                img = imresize(img, (120,120), interp='nearest' )
+                smallImages.append(img)
 
-#with open('accel.pkl','r') as f:
-#    data = pickle.load(f)
+        print( "{}Writing {} small images".format( indent, len(smallImages) ) )
 
-#with open('drive3.pickle', 'wb') as f3:
-#    pickle.dump( data, f3, protocol=pickle.HIGHEST_PROTOCOL )
-
-#data3 = { }
-#
-#print( data.keys() )
-#for key in data.keys():
-#    print( key )
-#
-#data = data3
-#print( "Image count: {}".format( len(data['images']) ) )
-#print( "Image actions count: {}".format( len(data['image_actions']) ) )
-#print( "Actions count: {}".format( len(data['actions']) ) )
-#
-#act = data['actions']
-#print( act )
+        with open(ofname,'wb') as f:
+            pickle.dump( smallImages, f, protocol=pickle.HIGHEST_PROTOCOL )
+    else:
+        print( "{}Small Images file already exists".format( indent ) )
 
 def writeToSeparateFiles():
     for key in ["action_times", "image_actions", "actions", "image_times", "images"]:
@@ -86,5 +92,103 @@ def readFromSeparateFiles():
         except:
             print( "{} Failed".format( key ) )
 
-#writeToSeparateFiles()
-#readFromSeparateFiles()
+def describeKey( key, key_data, indent="" ):
+    if isinstance(key_data, basestring):
+        print( "{}{}: {}".format( indent, key, key_data ) )
+    else:
+        key_len = 0
+        try:
+            key_len = len(key_data)
+        except:
+            pass
+        if key_len > 0:
+            print( "{}{} count: {}".format( indent, key, len(key_data) ) )
+        else:
+            print( "{}{}: {}".format( indent, key, str(key_data) ) )
+
+def describeData( data, indent="" ):
+    for key in data.keys():
+        describeKey( key, data[key], indent=indent )
+
+def sampleImages( drive_dir, data, indent="", count=5 ):
+
+    images = data['images']
+    with open(os.path.join( drive_dir, 'images_120x120.pickle' ),'r') as f:
+        small_images = pickle.load(f)
+
+    print( "Image array lengths (S/L): {}/{}".format( len(small_images), len(images) ) )
+
+    if len(images) == 0 or (len(images) != len(small_images)):
+        print( "Mismatched image array lengths" )
+        return
+
+    for i in range(count):
+        idx = np.random.randint(0,len(images))
+
+        img_large = images[idx]
+        img_small = small_images[idx]
+
+        plt.figure(1,figsize=(16, 18), dpi=80)
+        ax1=plt.subplot(211)
+        ax1.imshow(img_large)
+        ax1=plt.subplot(212)
+        ax1.imshow(img_small)
+
+        plt.show()
+
+
+def runTests(args, data):
+    pass
+
+def getOptions():
+
+    parser = argparse.ArgumentParser(description='View or post-process collected drive data.')
+    parser.add_argument('dirs', nargs='*', metavar="Directory", help='A directory containing recorded robot data')
+    parser.add_argument('-f', '--file', help='File with one directory per line')
+    parser.add_argument('--test_only', action="store_true", default=False, help='run tests, then exit')
+    parser.add_argument('--desc', action="store_true", default=False, help='Describe each drive, then exit')
+    parser.add_argument('--sample', action="store_true", default=False, help='Display sample images, then exit')
+
+    args = parser.parse_args()
+
+    if args.file is not None:
+        with open(args.file, "r") as f:
+            tmp_dirs = f.read().split('\n')
+            args.dirs.extend(tmp_dirs)
+
+    if len(args.dirs) == 0 and not args.test_only:
+        parser.print_help()
+        print( "\nNo directories supplied" )
+        exit()
+
+    return args
+
+if __name__ == "__main__":
+    args = getOptions()
+
+    indent="  "
+
+    if args.test_only:
+        runTests(args)
+        exit()
+
+    if args.sample:
+        data = loadDrive( args.dirs[0], indent=indent )
+        if data is not None:
+            sampleImages( args.dirs[0], data, indent=indent )
+        exit()
+        
+    for adir in args.dirs:
+        print( adir )
+        data = loadDrive( adir, indent=indent )
+
+        if data is not None:
+            if args.desc:
+                describeData( data, indent=indent )
+            else:
+                if not 'model' in data:
+                    print( "{}No model name in drive.pickle. Possibly an older drive format".format( indent ) )
+                else:
+                    print( "{}Model: {}".format( indent, data['model'] ) )
+                makeActions( adir, data, indent=indent )
+                makeSmallImages( adir, data, indent=indent )
