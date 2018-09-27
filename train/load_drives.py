31c27b5a833db195491084448f4bc903070dc6c7
Andrew Salamon <bleyddyn.aprhys@gmail.com>
2018-09-26 18:16:54 -0700

    Copied some drive loading code over from World Models. Added a fairly basic image augmentor class.

--- train/load_drives.py
+++ train/load_drives.py
@@ -14,6 +14,7 @@ from keras.utils import Sequence
 
 import malpiOptions
 from load_aux import getAuxFromMeta, loadOneAux
+from augmentor import ImageAugmentor
 
 # For python2/3 compatibility when calling isinstance(x,basestring)
 # From: https://stackoverflow.com/questions/11301138/how-to-check-if-variable-is-string-with-python-2-and-3-compatibility
@@ -78,7 +79,7 @@ def embedActions( actions ):
 class DriveDataGenerator(Sequence):
     """ Loads MaLPi drive data
         From: https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly.html"""
-    def __init__(self, filelist, image_size=(120,120), batch_size=32, shuffle=True, max_load=30000, auxName=None ):
+    def __init__(self, filelist, image_size=(120,120), batch_size=32, shuffle=True, max_load=30000, auxName=None, images_only=False ):
         """ Input a list of drive directories.
             Pre-load each to count number of samples.
             load one file and use it to generate batches until we run out.
@@ -92,6 +93,7 @@ class DriveDataGenerator(Sequence):
         self.shuffle = shuffle
         self.max_load = max_load
         self.auxName = auxName
+        self.images_only = images_only
         self.image_norm = False
         self.next_dir_index = 0
         self.images = []
@@ -100,6 +102,7 @@ class DriveDataGenerator(Sequence):
         self.categorical = None
         self.input_dim = None
         self.num_actions = None
+        self.batch_shape = (self.batch_size,) + self.size + (3,)
         self.count = self.__count()
         self.on_epoch_end()
 
@@ -113,15 +116,21 @@ class DriveDataGenerator(Sequence):
         sample_end = sample_beg + self.batch_size
         #print( "getitem {} {}:{}".format( index, sample_beg, sample_end ) )
 
-        if sample_end <= len(self.images):
+        if (sample_beg < len(self.images)) and (sample_end < len(self.images)):
             images = self.images[sample_beg:sample_end]
             actions = self.actions[sample_beg:sample_end]
-            return images, actions
+            if self.images_only:
+                if images.shape != self.batch_shape:
+                    print( "Invalid batch shape (1): {} {} {} {}".format( images.shape, sample_beg, sample_end, prev_len) )
+                return images
+            else:
+                return images, actions
 
         if sample_beg <= len(self.images):
             images = self.images[sample_beg:]
             actions = self.actions[sample_beg:]
-            sample_end = len(self.images) - sample_beg
+            #sample_end = len(self.images) - sample_beg
+            sample_end = self.batch_size - len(images)
             self.images, self.actions = self.__load_next_max()
             try:
                 i2 = self.images[0:sample_end]
@@ -137,7 +146,12 @@ class DriveDataGenerator(Sequence):
                 print( ex )
                 print( "actions {} {}".format( actions.shape, a2.shape ) )
                 print( "{}".format( actions ) )
-            return images, actions
+            if self.images_only:
+                if images.shape != self.batch_shape:
+                    print( "Invalid batch shape (2): {} {} {} {}".format( images.shape, sample_beg, sample_end, prev_len) )
+                return images
+            else:
+                return images, actions
         print( "Invalid batch indexes: {} {}:{}".format( index, sample_beg, sample_end ) )
 
     def __load_next_max(self):
@@ -155,9 +169,12 @@ class DriveDataGenerator(Sequence):
             self.next_dir_index += 1
 
         if self.shuffle == True:
-            images, actions = sklearn.utils.shuffle(images,actions)
+            if self.images_only:
+                images = sklearn.utils.shuffle(images)
+            else:
+                images, actions = sklearn.utils.shuffle(images,actions)
 
-        images = np.array(images)
+        images = np.array(images).astype(np.float) / 255.0
         actions = np.array(actions)
 
         if self.image_norm:
@@ -166,52 +183,54 @@ class DriveDataGenerator(Sequence):
         return images, actions
 
     def loadOneDrive( self, drive_dir, count_only=False ):
-        actions = None
-        if self.auxName is not None:
-            aux = getAuxFromMeta( drive_dir, self.auxName )
-            if aux is not None:
-                actions = loadOneAux( drive_dir, aux )
-        if actions is None:
-            actions_file = os.path.join( drive_dir, "image_actions.npy" )
-            if os.path.exists(actions_file):
-                actions = np.load(actions_file)
-            else:
-                actions_file = os.path.join( drive_dir, "image_actions.pickle" )
-                with open(actions_file,'r') as f:
-                    actions = pickle.load(f)
+        actions = []
+        if not self.images_only:
+            if self.auxName is not None:
+                aux = getAuxFromMeta( drive_dir, self.auxName )
+                if aux is not None:
+                    actions = loadOneAux( drive_dir, aux )
+            if len(actions) == 0:
+                actions_file = os.path.join( drive_dir, "image_actions.npy" )
+                if os.path.exists(actions_file):
+                    actions = np.load(actions_file)
+                else:
+                    actions_file = os.path.join( drive_dir, "image_actions.pickle" )
+                    with open(actions_file,'r') as f:
+                        actions = pickle.load(f)
 
-        categorical = True
-        if isinstance(actions[0], basestring):
-            actions = embedActions( actions )
-            actions = to_categorical( actions, num_classes=5 )
-            categorical = True
-        elif type(actions) == list:
-            actions = np.array(actions).astype('float')
-            categorical = False
-        elif type(actions) == np.ndarray:
-            actions = np.array(actions).astype('float')
-            categorical = False
-        else:
-            print("Unknown actions format: {} {} as {}".format( type(actions), actions[0], type(actions[0]) ))
+            if len(actions) > 0:
+                categorical = True
+                if isinstance(actions[0], basestring):
+                    actions = embedActions( actions )
+                    actions = to_categorical( actions, num_classes=5 )
+                    categorical = True
+                elif type(actions) == list:
+                    actions = np.array(actions).astype('float')
+                    categorical = False
+                elif type(actions) == np.ndarray:
+                    actions = np.array(actions).astype('float')
+                    categorical = False
+                else:
+                    print("Unknown actions format: {} {} as {}".format( type(actions), actions[0], type(actions[0]) ))
 
-        if self.categorical is None:
-            self.categorical = categorical
-        elif self.categorical != categorical:
-            print( "Mixed cat/non-cat action space: {}".format( drive_dir ) )
+                if self.categorical is None:
+                    self.categorical = categorical
+                elif self.categorical != categorical:
+                    print( "Mixed cat/non-cat action space: {}".format( drive_dir ) )
 
-        # Try training only the steering actions
-        actions = actions[:,0]
-        self.num_actions = 1
+                # Try training only the steering actions
+                actions = actions[:,0]
+                self.num_actions = 1
 
-        # Need an option for this
-        #if not self.categorical:
-        #    actions = self.addActionDiff(actions)
+                # Need an option for this
+                #if not self.categorical:
+                #    actions = self.addActionDiff(actions)
 
-        if self.num_actions is None:
-            self.num_actions = len(actions[0])
+                if self.num_actions is None:
+                    self.num_actions = len(actions[0])
 
-        if count_only:
-            return len(actions)
+                if count_only:
+                    return len(actions)
 
         basename = "images_{}x{}".format( self.size[0], self.size[1] )
         im_file = os.path.join( drive_dir, basename+".npy" )
@@ -222,7 +241,10 @@ class DriveDataGenerator(Sequence):
             with open(im_file,'r') as f:
                 images = pickle.load(f)
 
-        if len(images) != len(actions):
+        if count_only:
+            return len(images)
+
+        if not self.images_only and (len(images) != len(actions)):
             print( "Data mismatch: {}".format( drive_dir ) )
             print( "   images: {}".format( images.shape ) )
             print( "  actions: {}".format( actions.shape ) )
@@ -233,6 +255,8 @@ class DriveDataGenerator(Sequence):
         'Updates indexes after each epoch'
         if self.shuffle == True:
             np.random.shuffle(self.files)
+        self.images = []
+        self.actions = []
         self.next_dir_index = 0
         self.current_start = 0
         self.images, self.actions = self.__load_next_max()
@@ -251,7 +275,42 @@ class DriveDataGenerator(Sequence):
         return actions
 
 def runTests(args):
-    pass
+    import matplotlib.pyplot as plt
+
+    n = 10
+    gen = DriveDataGenerator(args.dirs, batch_size=n, shuffle=True, max_load=2000, images_only=True )
+
+    print( "# samples: {}".format( gen.count ) )
+    print( "# batches: {}".format( len(gen) ) )
+
+    images = gen[0]
+
+#    rmean = 92.93206363205326
+#    gmean = 85.80540021330793
+#    bmean = 54.14884297660608
+#    rstd = 57.696159704394354
+#    gstd = 53.739380109203445
+#    bstd = 47.66536771313241
+#
+#    images[:,:,:,0] *= rstd
+#    images[:,:,:,1] *= gstd
+#    images[:,:,:,2] *= bstd
+#    images[:,:,:,0] += rmean
+#    images[:,:,:,1] += gmean
+#    images[:,:,:,2] += bmean
+
+    aug = ImageAugmentor( 50.0 )
+    aug(images)
+
+    plt.figure(figsize=(20, 4))
+    plt.suptitle( "Sample Images", fontsize=16 )
+    for i in range(n):
+        ax = plt.subplot(2, n, i+1)
+        plt.imshow(images[i])
+        #plt.imshow(samples[i].reshape(input_dim[0], input_dim[1], input_dim[2]))
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    plt.show()
 
 def getOptions():
 
