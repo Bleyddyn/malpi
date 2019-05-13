b897b23046fddc178d55e6d2a9f697ef9b928486
Andrew Salamon <bleyddyn.aprhys@gmail.com>
2019-05-13 09:21:31 -0700

    Switched to using record dicts instead of arrays for TubFormat. Started adding support for aux data.

--- dagger/TubFormat.py
+++ dagger/TubFormat.py
@@ -45,40 +45,23 @@ class TubFormat(DriveFormat):
         self.tub = Tub(path)
         self.edit_list = set()
         self.shape = None
+        self.auxMeta = {}
         #(self.images, self.actions) = self._load(path)
 
     def _load( self, path, image_norm=True, progress=None ):
-        images = []
-        actions = []
 
-        num_records = self.tub.get_num_records()
-        for i in range(0,num_records+1):
-            try:
-                #print( "Loading {} of {}".format( i, num_records ), end='\r' )
-                if progress is not None:
-                    progress( i, num_records )
-                sys.stdout.flush()
-                rec = self.tub.get_record(i)
-                if 'cam/image_array' in rec:
-                    images.append( rec['cam/image_array'] )
-                    if self.shape is None:
-                        self.shape = rec['cam/image_array'].shape
-                    one_action = []
-                    for act in ["user/angle", "user/throttle"]:
-                        if act in rec:
-                            one_action.append( rec[act] )
-                    actions.append( one_action )
-                    #actions.append( rec["user/throttle"] )
-            except Exception as ex:
-                if i > 0 and i < num_records:
-                    # Some tubs are zero indexed, others 1. So this is only sometimes a real error
-                    print("Load failed: {}".format( ex ) )
-        #print("")
-
-        return images, actions
+        records = {}
+        indexes = self.tub.get_index(shuffled=False)
+        for idx in indexes:
+            rec = self.tub.get_record(idx)
+            if self.shape is None:
+                self.shape = rec['cam/image_array'].shape
+            records[idx] = rec
+        self.records = records
+        self.indexes = indexes
 
     def load( self, progress=None ):
-        (self.images, self.actions) = self._load(self.path, progress=progress)
+        self._load(self.path, progress=progress)
         self.setClean()
 
     def save( self ):
@@ -88,58 +71,74 @@ class TubFormat(DriveFormat):
         self.tub.write_exclude()
 
         for ix in self.edit_list:
-            #rec = self.tub.get_json_record(ix+1)
-            path = self.tub.get_json_record_path(ix+1)
-
-            # Had to duplicate this from Tub.get_json_record because that method also messes with paths in the record data
+            rec = self.records[ix]
+            path = self.tub.get_json_record_path(ix)
             try:
                 with open(path, 'r') as fp:
-                    rec = json.load(fp)
-            except UnicodeDecodeError:
-                raise Exception('bad record: %d. You may want to run `python manage.py check --fix`' % ix)
+                    old_rec = json.load(fp)
+            except TypeError:
+                print('troubles with record:', path)
             except FileNotFoundError:
                 raise
             except:
                 print("Unexpected error:", sys.exc_info()[0])
                 raise
 
-            new_actions = self.actions[ix]
+            # Copy over only the keys we might have modified, this will need to include aux data at some point.
+            for key in ['user/angle', 'user/throttle', 'orig/angle', 'orig/throttle']:
+                if key in rec:
+                    old_rec[key] = rec[key]
 
-            if (rec["user/angle"] != new_actions[0]) or (rec["user/throttle"] != new_actions[1]):
+            try:
+                with open(path, 'w') as fp: 
+                    json.dump(old_rec, fp)
+            except TypeError:
+                print('troubles with record:', path)
+            except FileNotFoundError:
+                raise
+            except:
+                print("Unexpected error:", sys.exc_info()[0])
+                raise
+
+        self.edit_list.clear()
+        self.setClean()
+
+    def count( self ):
+        return len(self.records)
+
+    def imageForIndex( self, index ):
+        idx = self.indexes[index]
+        img = self.records[idx]['cam/image_array']
+        if self.tub.excluded(index + 1):
+# This grayed out image ends up looking ugly, can't figure out why
+            tmp = img.mean(axis=-1,dtype=img.dtype,keepdims=False)
+            tmp = np.repeat( tmp[:,:,np.newaxis], 3, axis=2 )
+            return tmp
+        return img
+
+    def actionForIndex( self, index ):
+        idx = self.indexes[index]
+        rec = self.records[idx]
+        angle, throttle = self.tub.get_angle_throttle(rec)
+        return [angle, throttle]
+
+    def setActionForIndex( self, new_action, index ):
+        idx = self.indexes[index]
+        rec = self.records[idx]
+        angle, throttle = self.tub.get_angle_throttle(rec)
+        old_action = [angle, throttle]
+        if not np.array_equal( old_action, new_action ):
+            if (rec["user/angle"] != new_action[0]) or (rec["user/throttle"] != new_action[1]):
                 # Save the original values if not already done
                 if "orig/angle" not in rec:
                     rec["orig/angle"] = rec["user/angle"]
                 if "orig/throttle" not in rec:
                     rec["orig/throttle"] = rec["user/throttle"]
 
-                rec["user/angle"] = new_actions[0]
-                rec["user/throttle"] = new_actions[1]
-
-                with open(path, 'w') as fp:
-                    json.dump(rec, fp)
-
-        self.edit_list.clear()
-        self.setClean()
-
-    def count( self ):
-        return len(self.images)
-
-    def imageForIndex( self, index ):
-        if self.tub.excluded(index + 1):
-# This grayed out image ends up looking ugly, can't figure out why
-            tmp = self.images[index].mean(axis=-1,dtype=self.images[index].dtype,keepdims=False)
-            tmp = np.repeat( tmp[:,:,np.newaxis], 3, axis=2 )
-            return tmp
-        return self.images[index]
-
-    def actionForIndex( self, index ):
-        return self.actions[index]
-
-    def setActionForIndex( self, new_action, index ):
-        if not np.array_equal( self.actions[index], new_action ):
-            self.actions[index] = new_action
-            self.edit_list.add(index)
-            self.setDirty()
+                rec["user/angle"] = new_action[0]
+                rec["user/throttle"] = new_action[1]
+                self.edit_list.add(idx)
+                self.setDirty()
 
     def actionForKey(self,keybind,oldAction=None):
         oldAction = copy.copy(oldAction)
@@ -181,8 +180,29 @@ class TubFormat(DriveFormat):
             stats["StdDev"] = np.std(self.actions)
         return stats
 
+    def supportsAuxData(self):
+        return True
+
     def getAuxMeta(self):
-        return {}
+        return self.auxMeta
+
+    def addAuxData(self, meta):
+        # TODO Check to make sure the meta data is all the same
+        if meta["name"] not in self.auxMeta:
+            self.auxMeta[meta["name"]] = meta
+
+    def auxDataAtIndex(self, auxName, index):
+        if not auxName in self.auxData:
+            return None
+        return self.auxData[auxName][index]
+
+    def setAuxDataAtIndex(self, auxName, auxData, index):
+        if not auxName in self.auxData:
+            return False
+        if self.auxData[auxName][index] != auxData:
+            self.auxData[auxName][index] = auxData
+            self.setDirty()
+        return True
 
     @classmethod
     def canOpenFile( cls, path ):
@@ -206,8 +226,8 @@ class TubFormat(DriveFormat):
 
     def inputTypes(self):
         res = TubFormat.defaultInputTypes()
-        if len(self.images) > 0:
-            res[0]["shape"] = self.images[0].shape
+        if self.shape is not None:
+            res[0]["shape"] = self.shape
         return res
 
     @staticmethod
