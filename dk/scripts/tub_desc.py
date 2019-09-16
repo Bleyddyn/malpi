9afab1cfd57d34a9c049d52f22bc5cd8ddacc0bd
Andrew Salamon <bleyddyn.aprhys@gmail.com>
2019-09-15 20:32:30 -0700

    Added a time of day option to tub_desc output.

--- dk/scripts/tub_desc.py
+++ dk/scripts/tub_desc.py
@@ -2,7 +2,7 @@
 Describe Tub files found in one or more directories.
 
 Usage:
-    tub_desc.py [--file=<file> ...] [--stats] [<dir>...]
+    tub_desc.py [--file=<file> ...] [--stats] [--tod] [<dir>...]
 
 
 Options:
@@ -10,12 +10,14 @@ Options:
     <dir>            One or more directories that are Tub files, or contain Tub files.
     -f --file=<file> A text file containing paths to tub files, one per line. Option may be used more than once.
     -s --stats       Calculate mean and standard deviation for throttle. [Default: False]
+    --tod            Display time of day for beginning of each tub file. [Default: False]
 
 """
 import os
 import time
 from docopt import docopt
 import json
+from datetime import datetime
 
 import numpy as np
 import donkeycar as dk
@@ -40,7 +42,7 @@ def preprocessFileList( filelist ):
     removeComments( dirs )
     return dirs
 
-def describe_tub( tub_dir, stats=False ):
+def describe_tub( tub_dir, stats=False, time_of_day=False ):
     """ TODO: This should be generalized to return only user-requested meta data.
     """
     tub = Tub(tub_dir)
@@ -48,6 +50,7 @@ def describe_tub( tub_dir, stats=False ):
     loc = tub.meta.get("location", "NA")
     task = tub.meta.get("task", "NA")
     driver = tub.meta.get("driver", "NA")
+    tod = tub.meta.get("start", None)
     throttle = tub.meta.get("JOYSTICK_MAX_THROTTLE", "NA")
     steering = tub.meta.get("JOYSTICK_STEERING_SCALE", "NA")
     if stats:
@@ -63,14 +66,23 @@ def describe_tub( tub_dir, stats=False ):
         st = "\t{:0.3}/{:0.3}".format( float(thr_m), float(thr_v) )
     else:
         st = ""
-    print( "{}\t{}\t{}\t{}\t{}\t{}\t{}{}".format( os.path.basename(tub_dir), count, loc, task, driver, throttle, steering, st ) )
+
+    if time_of_day:
+        if tod is not None:
+            tod = int(tod)
+            tod = datetime.fromtimestamp(tod).strftime('\t%H:%M')
+        else:
+            tod = "\t"
+    else:
+        tod = ""
+    print( "{}\t{}\t{}\t{}\t{}\t{}\t{}{}{}".format( os.path.basename(tub_dir), count, loc, task, driver, throttle, steering, st, tod ) )
     return count
 
-def check_path( apath, counter=None, stats=False ):
+def check_path( apath, counter=None, stats=False, time_of_day=False ):
     if os.path.isdir(apath):
         meta_path = os.path.join( apath, "meta.json" )
         if os.path.exists(meta_path):
-            cnt = describe_tub(apath, stats)
+            cnt = describe_tub(apath, stats, time_of_day)
             if counter is not None:
                 counter.append( cnt )
             return True
@@ -86,6 +98,7 @@ if __name__ == "__main__":
         cfg = dk.load_config("config.py")
 
     do_stats = args['--stats']
+    do_tod = args['--tod']
     dirs = preprocessFileList( args['--file'] )
     dirs.extend( args['<dir>'] )
     dirs = [os.path.expanduser(adir) for adir in dirs]
@@ -96,14 +109,18 @@ if __name__ == "__main__":
         stat_str = "\tMean/Std"
     else:
         stat_str = ""
-    print( "\nTub\t# Samples\tLocation\tTask\tDriver\tThrottle\tSteering{}".format( stat_str ) )
-    for adir in dirs:
+    if do_tod:
+        tod_header = "\tTime"
+    else:
+        tod_header = ""
+    print( "\nTub\t# Samples\tLocation\tTask\tDriver\tThrottle\tSteering{}{}".format( stat_str, tod_header ) )
+    for adir in sorted(dirs):
         if adir not in done and os.path.isdir(adir):
-            if not check_path(adir, counter=counts, stats=do_stats):
+            if not check_path(adir, counter=counts, stats=do_stats, time_of_day=do_tod):
                 for afile in os.listdir(adir):
                     fpath = os.path.join(adir, afile)
                     if fpath not in done:
-                        check_path( fpath, counter=counts, stats=do_stats )
+                        check_path( fpath, counter=counts, stats=do_stats, time_of_day=do_tod )
                         done.append(fpath)
             else:
                 done.append(adir)
