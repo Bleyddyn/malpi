fbafe56ada4b983ee79b32ef7af85f5984947969
Andrew Salamon <bleyddyn.aprhys@gmail.com>
2019-09-29 11:42:55 -0700

    DK train script now supports Malpi Experiments to collect meta-data. The Experiment class can handle argparse or docopt objects.

--- dk/scripts/train_dk.py
+++ dk/scripts/train_dk.py
@@ -10,21 +10,27 @@ Basic usage should feel familiar: python train.py --model models/mypilot
 The --email option defaults to None (docopt doesn't seem to handle that case explicitly)
 
 Usage:
-    train.py [--tub=<tub1,tub2,..tubn>] [--file=<file> ...] (--model=<model>) [--transfer=<model>] [--type=(linear|latent|categorical|rnn|imu|behavior|3d|look_ahead|tensorrt_linear|tflite_linear|coral_tflite_linear)] [--continuous] [--aug] [--email=<email>]
+    train.py [--tub=<tub1,tub2,..tubn>] [--file=<file> ...] (--model=<model>) [--transfer=<model>] [--type=(linear|latent|categorical|rnn|imu|behavior|3d|look_ahead|tensorrt_linear|tflite_linear|coral_tflite_linear)] [--continuous] [--aug] [--email=<email>] [--name=<exp_name>] [--exp=<exp_dir>]
 
 Options:
-    -h --help        Show this screen.
-    -f --file=<file> A text file containing paths to tub files, one per line. Option may be used more than once.
-    --email=<email>  Email address to send training finished notifications. Requires email configuration file.
+    -h --help         Show this screen.
+    -f --file=<file>  A text file containing paths to tub files, one per line. Option may be used more than once.
+    --email=<email>   Email address to send training finished notifications. Requires email configuration file.
+    --name=<exp_name> Name for this training experiment.
+    --exp=<exp_dir>   Top level experiment directory. [default: experiments]
 """
 
 import os
 from docopt import docopt
 
+import numpy as np
+import tensorflow as tf
+
 import donkeycar as dk
 from donkeycar.train.train import preprocessFileList, train, make_model, Generators, plot_history
 
 from malpi.notify import notify, read_email_config
+from malpi import Experiment
 
 if __name__ == "__main__":
     args = docopt(__doc__)
@@ -44,8 +50,6 @@ if __name__ == "__main__":
     if model_type is None:
         model_type = cfg.DEFAULT_MODEL_TYPE
 
-    cfg.MAX_EPOCHS = 2
-
     kl, model_name = make_model(cfg, model, transfer, model_type )
 
     gens = Generators( cfg, dirs, kl, model_type, continuous, aug=aug )
@@ -55,7 +59,14 @@ if __name__ == "__main__":
     print("Samples: total: %d, train: %d, val: %d" % (gens.total_count(), gens.train_count(), gens.val_count()))
     print('Steps per epoch: %d' % gens.train_steps())
 
+    exp = None
+    if args['--name'] is not None:
+        exp = Experiment( args['--name'], args, exp_dir=args['--exp'], num_samples=gens.total_count(), input_dim=(cfg.TARGET_H,cfg.TARGET_W,cfg.TARGET_D), hparams=cfg.__dict__, modules=[np, tf, dk] )
+
     history = train(kl, cfg, gens, model_name, continuous, verbose=cfg.VEBOSE_TRAIN)
+
+    if exp is not None:
+        exp.writeAfter( model=kl.model, histories=history.history, results={"model_path": model_name} )
 
     try:
         notifications = read_email_config()
